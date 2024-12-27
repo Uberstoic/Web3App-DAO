4b38c1222a2e11f8b2d5f5debf5ff875fffe21c7
uberstoic <serjpvlv@gmail.com>
2024-12-27 21:21:40 +0300

    svg update

--- dao-frontend/tailwind.config.js
+++ dao-frontend/tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/metamask.svg",
+    "./public/walletconnect.svg",
     "./public/index.html",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'metamask': "url('/metamask.svg')",
-        'walletconnect': "url('/walletconnect.svg')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
